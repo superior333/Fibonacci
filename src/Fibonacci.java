bf03f1a6be26ce9cbc4c10c7baa9e3bf5f9a76d5
Homework 1(Fibonacci) <naby20099@gmail.com>
2025-11-02 12:31:07 +0300

    Final result.

---
+++ src/Fibonacci.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите количество чисел Фибоначчи для вывода: ");
+        int n = scanner.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Пожалуйста, введите положительное число.");
+            return;
+        }
+
+        System.out.println("Первые " + n + " чисел Фибоначчи:");
+
+        int first = 0, second = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        scanner.close();
+    }
+}
